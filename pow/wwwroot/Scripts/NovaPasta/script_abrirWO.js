f578373bee7d6a966c54bce6cba4d37b22820fd3
WESLEY SANTOS <Junior_Wesley@jabil.com>
2022-03-07 08:14:35 -0400

    Adicionar arquivos de projeto.

---
+++ pow/wwwroot/Scripts/NovaPasta/script_abrirWO.js
@@ -0,0 +1,45 @@
+﻿  // $(document).ready(function () {
+   //  $('#btnNew').on('click', function () {
+
+        // if (verificarCampos() == 1) {
+      //       $('#form').submit();
+       //  }
+   //  });
+
+   //  function verificarCampos() {
+    //     var dpt = $('#dpt').val();
+     //    var imp = $('#imp').val();
+     //    var svr = $('#svr').val();
+    //     var tlt = $('#tlt').val();
+      //   var dsc = $('#dsc').val();
+
+     //    if (dpt == 0) {
+      //       alert('Fill out the field Department');
+     //    }
+      //   else {
+       //      if (imp == 0) {
+        //         alert('Fill out the field Impact Issue');
+        //     }
+         //    else {
+          //       if (svr == 0) {
+            //         alert('Fill out the field Several Impact');
+           //      }
+           //      else {
+             //        if (tlt == "") {
+               //          alert('Fill out the Work Order Title');
+                 //    }
+                  //   else {
+                  //       if (dsc == "") {
+                   //          alert('Fill out the Work Order Description');
+                   //      }
+                    //     else {
+                    //         return 1;
+                 //        }
+             //        }
+           //      }
+          //   }
+      //   }
+// 
+     //    return 0;
+ //    }
+//  });
---
+++ pow/wwwroot/Scripts/NovaPasta/script_fecharWQ.js
@@ -0,0 +1,26 @@
+﻿// $(document).ready(function () {
+   // $('#btnFechar').on('click', function () {
+       //  var obs = $('#txtObs').val();
+
+       //  if (obs == '') {
+       //      alert("Fill out the field Resolution's notes");
+// }
+// else {
+// $('#ObservationDesc').val(obs);
+// $('#form').submit();
+        }
+// });
+
+// $('#btnAtualizar').on('click', function () {
+// var obs = $('#txtAssign').val();
+
+// if (obs == '0') {
+// alert("Fill out the field Assign");
+// }
+// else {
+// $('#userId').val(obs);
+// $('#formAtualizar').submit();
+// }
+// });
+ // });
+
